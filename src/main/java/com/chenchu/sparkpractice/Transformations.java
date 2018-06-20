f5d3c0bb7b29a28df1fd6933efd827cc1c97ce63
chenchusubbareddy <chenchu9618254711@gmail.com>
2018-06-20 20:28:39 +0530

    first commit

---
+++ src/main/java/com/chenchu/sparkpractice/Transformations.java
@@ -0,0 +1,21 @@
+package com.chenchu.sparkpractice;
+
+import java.util.Arrays;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+public class Transformations {
+	static SparkSession ss = SparkSession.builder().appName("spark-practice").master("local[*]").getOrCreate();
+	static JavaSparkContext jsc = new JavaSparkContext(ss.sparkContext());
+	static JavaRDD<Integer> irdd = jsc.parallelize(Arrays.asList(1,2,3,4,5,6,7,8,9));
+	
+	public static void main(String[] args) {
+		jsc.setLogLevel("OFF");
+		System.out.println(ss);
+		System.out.println(jsc);
+		
+		
+	}
+}
