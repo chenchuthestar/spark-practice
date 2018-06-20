b7012b95e3cbb6e024fae170a9363e901ccbf540
chenchusubbareddy <chenchu9618254711@gmail.com>
2018-06-20 22:00:21 +0530

    some new transfermations added

--- src/main/java/com/chenchu/sparkpractice/Transformations.java
+++ src/main/java/com/chenchu/sparkpractice/Transformations.java
@@ -1,23 +1,94 @@
 package com.chenchu.sparkpractice;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.sql.SparkSession;
 
 public class Transformations {
 	static SparkSession ss = SparkSession.builder().appName("spark-practice").master("local[*]").getOrCreate();
 	static JavaSparkContext jsc = new JavaSparkContext(ss.sparkContext());
-	static JavaRDD<Integer> irdd = jsc.parallelize(Arrays.asList(1,2,3,4,5,6,7,8,9));
-	
+	static JavaRDD<Integer> irdd = jsc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	static JavaRDD<String> SRdd = jsc
+			.parallelize(Arrays.asList("I am going", "to hyd", "I am learning", "hadoop course"));
+
 	public static void main(String[] args) {
 		jsc.setLogLevel("OFF");
 		System.out.println(ss);
 		System.out.println(jsc);
-		JavaRDD<Integer> mapRdd = irdd.map(x -> x+1);
+		System.out.println("rdd partitions:" + irdd.getNumPartitions());
+		// map
+		JavaRDD<Integer> mapRdd = irdd.map(x -> x + 1);
 		System.out.println(mapRdd.collect());
 
+		// filter
+		JavaRDD<Integer> filterRdd = irdd.filter(x -> x > 1);
+		System.out.println(filterRdd.collect());
+
+		// flatmap
+		JavaRDD<String> flatmapRdd = SRdd.flatMap(str -> Arrays.asList(str.split(" ")).iterator());
+		System.out.println(flatmapRdd.collect());
+
+		// mappartitions
+		JavaRDD<Integer> mapPartitionsRdd = irdd.mapPartitions(itr -> Arrays.asList(itr.next()).iterator());
+		System.out.println(mapPartitionsRdd.collect());
+
+		Function2<Integer, Iterator<Integer>, Iterator<String>> fun = (i, itr) -> {
+			System.out.print(i + "    ");
+			while (itr.hasNext()) {
+				System.out.print(itr.next() + ",");
+			}
+
+			System.out.println();
+			return Arrays.asList("Abc").iterator();
+		};
+		JavaRDD<String> mapPartitionsWithIndexRdd = irdd.mapPartitionsWithIndex(fun, true);
+		System.out.println(mapPartitionsWithIndexRdd.collect());
+		System.out.println("==============================sample() Example==========================");
+		JavaRDD<Integer> sampleRdd = irdd.sample(true, 0.5);
+		System.out.println(sampleRdd.collect());
+
+		JavaRDD<Integer> sampleRdd1 = irdd.sample(false, 0.5);
+		System.out.println(sampleRdd1.collect());
 		
+		
+		System.out.println("=================union example========================================");
+		
+		JavaRDD<Integer> unionRdd = irdd.union(jsc.parallelize(Arrays.asList(9,8,7)));
+		System.out.println(unionRdd.collect());
+		
+		System.out.println("=================intersection================================");
+		JavaRDD<Integer> intersectionRdd = irdd.intersection(jsc.parallelize(Arrays.asList(1,2,3,4,5)));
+		System.out.println(intersectionRdd.collect());
+		
+		System.out.println("==================distinct=================================");
+		
+		JavaRDD<Integer> distinctRdd = jsc.parallelize(Arrays.asList(1,2,3,4,5,4,3)).distinct();
+		System.out.println(distinctRdd.collect());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 	}
 }
