62ab5bdb4e6c9a3030058f5ae4c3d37fb106eed1
chenchusubbareddy <chenchu9618254711@gmail.com>
2018-06-26 17:56:52 +0530

    +-local commit

--- src/main/java/com/chenchu/sparkpractice/Transformations.java
+++ src/main/java/com/chenchu/sparkpractice/Transformations.java
@@ -4,18 +4,22 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.sql.SparkSession;
 
+import scala.Tuple2;
+
 public class Transformations {
 	static SparkSession ss = SparkSession.builder().appName("spark-practice").master("local[*]").getOrCreate();
 	static JavaSparkContext jsc = new JavaSparkContext(ss.sparkContext());
 	static JavaRDD<Integer> irdd = jsc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-	static JavaRDD<String> SRdd = jsc
+	static JavaRDD<String> sRdd = jsc
 			.parallelize(Arrays.asList("I am going", "to hyd", "I am learning", "hadoop course"));
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		jsc.setLogLevel("OFF");
 		System.out.println(ss);
@@ -30,7 +34,7 @@ public class Transformations {
 		System.out.println(filterRdd.collect());
 
 		// flatmap
-		JavaRDD<String> flatmapRdd = SRdd.flatMap(str -> Arrays.asList(str.split(" ")).iterator());
+		JavaRDD<String> flatmapRdd = sRdd.flatMap(str -> Arrays.asList(str.split(" ")).iterator());
 		System.out.println(flatmapRdd.collect());
 
 		// mappartitions
@@ -54,29 +58,29 @@ public class Transformations {
 
 		JavaRDD<Integer> sampleRdd1 = irdd.sample(false, 0.5);
 		System.out.println(sampleRdd1.collect());
-		
-		
+
 		System.out.println("=================union example========================================");
-		
-		JavaRDD<Integer> unionRdd = irdd.union(jsc.parallelize(Arrays.asList(9,8,7)));
+
+		JavaRDD<Integer> unionRdd = irdd.union(jsc.parallelize(Arrays.asList(9, 8, 7)));
 		System.out.println(unionRdd.collect());
-		
+
 		System.out.println("=================intersection================================");
-		JavaRDD<Integer> intersectionRdd = irdd.intersection(jsc.parallelize(Arrays.asList(1,2,3,4,5)));
+		JavaRDD<Integer> intersectionRdd = irdd.intersection(jsc.parallelize(Arrays.asList(1, 2, 3, 4, 5)));
 		System.out.println(intersectionRdd.collect());
-		
+
 		System.out.println("==================distinct=================================");
-		
-		JavaRDD<Integer> distinctRdd = jsc.parallelize(Arrays.asList(1,2,3,4,5,4,3)).distinct();
+
+		JavaRDD<Integer> distinctRdd = jsc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 4, 3)).distinct();
 		System.out.println(distinctRdd.collect());
+
+		System.out.println("===================== tuple rdd==============");
 		
-		
-		
-		
-		
-		
-		
-		
+		JavaRDD<String> flatMap = sRdd.flatMap(str -> Arrays.asList(str.split(" ")).iterator());
+		JavaPairRDD<String,Integer> mapToPair = flatMap.mapToPair(str -> new Tuple2<String,Integer>(str,1));
+		JavaPairRDD<String,Iterable<Integer>> groupByKey = mapToPair.groupByKey();
+		System.out.println(groupByKey.collect());
+		JavaPairRDD<String,Integer> reduceByKey = mapToPair.reduceByKey((a ,b ) -> a+b );
+		System.out.println(reduceByKey.collect());
 		
 		
 		
